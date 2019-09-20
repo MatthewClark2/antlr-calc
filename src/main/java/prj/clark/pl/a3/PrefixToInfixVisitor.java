48001c947a44539768f3610c6195d737df556509
Matthew Clark <matthew.clark.dev@gmail.com>
2019-09-20 13:55:56 -0500

    Add basic prefix to infix conversion and tests.

---
+++ src/main/java/prj/clark/pl/a3/PrefixToInfixVisitor.java
@@ -0,0 +1,62 @@
+package prj.clark.pl.a3;
+
+public class PrefixToInfixVisitor extends PrefixMathBaseVisitor<String> {
+    private StringBuilder output;
+
+    public PrefixToInfixVisitor() {
+        output = new StringBuilder();
+    }
+
+
+    @Override
+    public String visitFile(PrefixMathParser.FileContext ctx) {
+        for (PrefixMathParser.StmtContext sctx : ctx.stmt()) {
+            visit(sctx);
+            output.append("\n");
+        }
+
+        return output.toString();
+    }
+
+    @Override
+    public String visitAssignment(PrefixMathParser.AssignmentContext ctx) {
+        output.append(ctx.ID().getText())
+                .append(" = ");
+        visit(ctx.expr());
+
+        return output.toString();
+    }
+
+    @Override
+    public String visitOperation(PrefixMathParser.OperationContext ctx) {
+        visit(ctx.expr(0));
+        output.append(" ")
+                .append(ctx.op.getText())
+                .append(" ");
+        visit(ctx.expr(1));
+
+        return output.toString();
+    }
+
+    @Override
+    public String visitNegative(PrefixMathParser.NegativeContext ctx) {
+        output.append("-");
+        visit(ctx.expr());
+
+        return output.toString();
+    }
+
+    @Override
+    public String visitNum(PrefixMathParser.NumContext ctx) {
+        output.append(ctx.getText());
+
+        return output.toString();
+    }
+
+    @Override
+    public String visitId(PrefixMathParser.IdContext ctx) {
+        output.append(ctx.ID().getText());
+
+        return output.toString();
+    }
+}
---
+++ src/test/java/prj/clark/pl/a3/PrefixToInfixVisitorTest.java
@@ -0,0 +1,46 @@
+package prj.clark.pl.a3;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PrefixToInfixVisitorTest {
+    private String p2i(String input) {
+        CharStream cs = CharStreams.fromString(input + "\n");
+        MathLexer lex = new MathLexer(cs);
+        CommonTokenStream cts = new CommonTokenStream(lex);
+        PrefixMathParser parse = new PrefixMathParser(cts);
+
+        PrefixToInfixVisitor prefixToInfix = new PrefixToInfixVisitor();
+        return prefixToInfix.visitFile(parse.file()).trim();
+    }
+
+    @Test
+    public void singleValue() {
+        assertEquals("a", p2i("a"));
+    }
+
+    @Test
+    public void singleExpression() {
+        assertEquals("a = b", p2i("= a b"));
+    }
+
+    @Test
+    public void orderOfOperationsCarriesThrough() {
+        // (1 + 3) * 2 -> * + 1 3 2
+        assertEquals("(1 + 3) * 2", p2i("* + 1 3 2"));
+    }
+
+    @Test
+    public void stackedExpressions() {
+        assertEquals("((1 + 2) - 3) / 4 * 5", p2i("* / - + 1 2 3 4 5"));
+    }
+
+    @Test
+    public void multipleLines() {
+        assertEquals("a = b\nb + 10\nc / 2 + 3", p2i("= a b\n+ b 10\n+ / c 2 3"));
+    }
+}
